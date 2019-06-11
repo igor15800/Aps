6139e8f6dc0193b0b31726b0dfb954b285b7215a
Igor Felipe da Silva Gomes <igor.felipe@labs.dcx>
2019-06-11 11:38:21 -0300

    mensagem

--- src/exercicioaps/App.java
+++ src/exercicioaps/App.java
@@ -1,5 +1,12 @@
 package exercicioaps;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
+	
+	private Map<Integer,Marcas> marcas = new HashMap<Integer, Marcas>();
+	
+	
 
 }
--- src/exercicioaps/MarcaService.java
+++ src/exercicioaps/MarcaService.java
@@ -1,5 +1,10 @@
 package exercicioaps;
 
+import java.util.List;
+import java.util.ArrayList;
 public class MarcaService {
+	private List<Marcas> marca = new ArrayList<Marcas>();
+	
+	
 
 }
